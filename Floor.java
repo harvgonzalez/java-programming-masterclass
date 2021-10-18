4ac6af5f0edbb4894a49c5977184bb623fe73ad4
Javier Gonzalez <69843590+harvgonzalez@users.noreply.github.com>
2021-10-18 11:15:16 +0200

    Add files via upload

---
+++ Floor.java
@@ -0,0 +1,24 @@
+public class Floor {
+    private double width;
+    private double length;
+
+    // Constructor
+    public Floor(double width, double length) {
+ 
+        if (width < 0)
+            this.width = 0;
+        else
+            this.width = width;
+ 
+        if (length < 0)
+            this.length = 0;
+        else
+            this.length = length;
+ 
+    }
+
+        // Methods
+    public double getArea() {
+        return width * length;
+    }
+}
